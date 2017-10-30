ec32137f2285ea089726c08bc9351174369260b1
B0544139 <jon971997@yahoo.com.tw>
2017-10-30 15:29:44 +0800

    B0544139 HW2

--- HW2.java
+++ HW2.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * @author TODO: please add student ID and name here
+ * @author TODO: please add student ID and name here B0544139 林元復
  * Try to write some comments for your codes (methods, 15 points)
  */
 public class HW2 {
@@ -77,12 +77,29 @@ class Deck{
 		//Card card=new Card(1,1); ->means new card as clubs ace
 		//cards.add(card);
 		//Sample code end
-
+		for(int num1=1;num1<=nDeck;num1++)//幾副牌
+        {
+        	for(int x=1;x<=4;x++)//花色
+        	{
+        		 for(int y=1;y<=13;y++)//數字
+        		 {
+        				Card card=new Card(x,y);//將card實體化
+        				cards.add(card);//將card放入ArrayList<Card>裡
+        		 }
+        	}
+        }
+		
+		
 	}	
 	//TODO: Please implement the method to print all cards on screen (10 points)
 	public void printDeck(){
 		//Hint: print all items in ArrayList<Card> cards, 
 		//TODO: please implement and reuse printCard method in Card class (5 points)
+		for(int index=0;index<cards.size();index++)
+		{
+			Card card = new Card(cards.get(index).getSuit(),cards.get(index).getRank());//將卡片及花色實體化
+			card.printCard();//將所有卡顯示出來
+		}
 
 	}
 	public ArrayList<Card> getAllCards(){
@@ -106,7 +123,14 @@ class Card{
 	//TODO: 1. Please implement the printCard method (20 points, 10 for suit, 10 for rank)
 	public void printCard(){
 		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
-
+		String[] type1={"Clubs","Diamonds","Hearts","Spades"};//所有的花色
+        String[] number1={"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+        		       "Jack","Queen","King"};//所有的數字
+        String type=Integer.toString(suit);
+        String number=Integer.toString(rank);
+        type=type1[suit-1];//將輸入轉換為對應到的花色
+        number=number1[rank-1];//將輸入轉換為對應到的數字
+		System.out.println(type+" "+number);//將花色及數字顯示出來
 	}
 	public int getSuit(){
 		return suit;
